aaeac321d536e7ef8669dcd0e8d6c7dcfc78c907
Adam Budzik <abudzik@networkedassets.org>
2024-09-28 10:14:23 +0200

    feat: Start game feature

--- src/main/kotlin/io/budzik/scoreboard/Scoreboard.kt
+++ src/main/kotlin/io/budzik/scoreboard/Scoreboard.kt
@@ -3,12 +3,28 @@ package io.budzik.scoreboard
 import io.budzik.scoreboard.model.Game
 import io.budzik.scoreboard.model.GameId
 import org.slf4j.LoggerFactory
+import java.time.Instant
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
 
 class Scoreboard {
+    private val games: ConcurrentHashMap<GameId, Game> = ConcurrentHashMap()
+    private val gameIdCounter: AtomicInteger = AtomicInteger(0)
     private val log = LoggerFactory.getLogger(Scoreboard::class.java)
 
     fun startGame(homeTeam: String, awayTeam: String): Game {
-        TODO()
+        val id = gameIdCounter.incrementAndGet()
+        val game = Game(
+            homeTeam = homeTeam,
+            awayTeam = awayTeam,
+            homeScore = 0,
+            awayScore = 0,
+            startTime = Instant.now(),
+            gameId = id
+        )
+        games[id] = game
+        log.debug("Created a game with ID [$id]: [$game]")
+        return game
     }
 
     fun updateGame(gameId: GameId, homeScore: Short, awayScore: Short): Game {
@@ -19,7 +35,5 @@ class Scoreboard {
         TODO()
     }
 
-    fun getSummary(): List<Game> {
-        TODO()
-    }
+    fun getSummary(): List<Game> = games.values.toList()
 }
